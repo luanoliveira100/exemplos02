3fe81728a3b4e8bb1112d72261e64288cf21cf3f
luan <luanoliveira100@hotmail.com>
2018-06-27 20:51:26 -0300

    crud exemplo entity framework

---
+++ ex01/ex01/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ex01.Startup))]
+namespace ex01
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
